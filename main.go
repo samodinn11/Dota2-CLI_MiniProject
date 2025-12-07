cfec980f065c965ebb245c00540c6888f1213513
NorvinCodes <norvin.codes@gmail.com>
2025-12-07 18:24:05 +0000

    structs

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// HERO STRUCT
+type Hero struct {
+	Name  string
+	Role  string
+	Power int
+}
+
 func main() {
 	var userChoice int
 
@@ -24,24 +31,25 @@ func main() {
 			fmt.Println("Invalid option")
 		}
 	}
-
 }
 
 func showMenu() {
-	fmt.Println("1) Show Hero: ")
-	fmt.Println("2) Show Version: ")
-	fmt.Println("3) Exit: ")
+	fmt.Println("1) Show Hero")
+	fmt.Println("2) Show Version")
+	fmt.Println("3) Exit")
 }
 
-// INFO ABT HERO
+// INFO ABOUT HERO (NOW USING STRUCT & STRUCT LITERAL)
 func heroInfo() {
-	name := "Witch Doctor"
-	role := "Support"
-	power := 0
+	hero := Hero{
+		Name:  "Witch Doctor",
+		Role:  "Support",
+		Power: 0,
+	}
 
-	fmt.Println("Hero: ", name)
-	fmt.Println("Role: ", role)
-	fmt.Println("Power: ", power)
+	fmt.Println("Hero:", hero.Name)
+	fmt.Println("Role:", hero.Role)
+	fmt.Println("Power:", hero.Power)
 }
 
 // INFO ABOUT APP
@@ -49,18 +57,28 @@ func appInfo() {
 	version := "1.0"
 	patch := "Test Patch"
 	fmt.Println("---------------------")
-	fmt.Println("Version: ", version, ", Patch: ", patch)
+	fmt.Println("Version:", version)
+	fmt.Println("Patch:", patch)
 	fmt.Println("---------------------")
 }
 
-// WELCOME & EXIT MESSAGE
+// WELCOME MESSAGE
 func welcomeMessage() {
 	fmt.Println("WELCOME TO DOTA2 CLI!")
 	fmt.Println("---------------------")
-
 }
+
+// EXIT MESSAGE
 func exitMessage() {
 	fmt.Println("---------------------")
 	fmt.Println("Thank you commander!")
-	fmt.Println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n▒██▄▒▒▒▒▒▒▒▀██▒▒\n▒▒▀██▄▒▒▒▒▒▒▒▀▒▒\n▒▒▒▒▀██▄▒▒▒▒▒▒▒▒\n▒▒▒▒▒▒▀██▄▄▒▒▒▒▒\n▒▒▒▒▒▒▒▀████▄▒▒▒\n▒▒█▄▒▒▒▒▒▀███▒▒▒\n▒▒▀▀▀▒▒▒▒▒▒▒▒▒▒▒\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	fmt.Println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒")
+	fmt.Println("▒██▄▒▒▒▒▒▒▒▀██▒▒")
+	fmt.Println("▒▒▀██▄▒▒▒▒▒▒▒▀▒▒")
+	fmt.Println("▒▒▒▒▀██▄▒▒▒▒▒▒▒▒")
+	fmt.Println("▒▒▒▒▒▒▀██▄▄▒▒▒▒▒")
+	fmt.Println("▒▒▒▒▒▒▒▀████▄▒▒▒")
+	fmt.Println("▒▒█▄▒▒▒▒▒▀███▒▒▒")
+	fmt.Println("▒▒▀▀▀▒▒▒▒▒▒▒▒▒▒▒")
+	fmt.Println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒")
 }
