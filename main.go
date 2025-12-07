411c8ceee1f5227db3921dff08c5f65180a9aa39
NorvinCodes <norvin.codes@gmail.com>
2025-12-07 15:50:54 +0000

    Add interactive menu with hero and version options

--- main.go
+++ main.go
@@ -3,18 +3,34 @@ package main
 import "fmt"
 
 func main() {
+	var userChoice int
+
 	welcomeMessage()
-	heroInfo()
-	appInfo()
-	exitMessage()
+
+	for {
+		showMenu()
+		fmt.Scan(&userChoice)
+		fmt.Println("You Chose Option:", userChoice)
+
+		switch userChoice {
+		case 1:
+			heroInfo()
+		case 2:
+			appInfo()
+		case 3:
+			exitMessage()
+			return
+		default:
+			fmt.Println("Invalid option")
+		}
+	}
 
 }
 
-// WELCOME MESSAGE
-func welcomeMessage() {
-	fmt.Println("WELCOME TO DOTA2 CLI!")
-	fmt.Println("---------------------")
-
+func showMenu() {
+	fmt.Println("1) Show Hero: ")
+	fmt.Println("2) Show Version: ")
+	fmt.Println("3) Exit: ")
 }
 
 // INFO ABT HERO
@@ -28,13 +44,23 @@ func heroInfo() {
 	fmt.Println("Power: ", power)
 }
 
+// INFO ABOUT APP
 func appInfo() {
 	version := "1.0"
 	patch := "Test Patch"
 	fmt.Println("---------------------")
 	fmt.Println("Version: ", version, ", Patch: ", patch)
+	fmt.Println("---------------------")
+}
+
+// WELCOME & EXIT MESSAGE
+func welcomeMessage() {
+	fmt.Println("WELCOME TO DOTA2 CLI!")
+	fmt.Println("---------------------")
+
 }
 func exitMessage() {
 	fmt.Println("---------------------")
 	fmt.Println("Thank you commander!")
+	fmt.Println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n▒██▄▒▒▒▒▒▒▒▀██▒▒\n▒▒▀██▄▒▒▒▒▒▒▒▀▒▒\n▒▒▒▒▀██▄▒▒▒▒▒▒▒▒\n▒▒▒▒▒▒▀██▄▄▒▒▒▒▒\n▒▒▒▒▒▒▒▀████▄▒▒▒\n▒▒█▄▒▒▒▒▒▀███▒▒▒\n▒▒▀▀▀▒▒▒▒▒▒▒▒▒▒▒\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
 }
